e69f96885ee99ff68f3487590852aa5ab1050ad5
CHOUDHARY KHIMESH BHAWARLAL DAMYANTI <133648905+KhimeshChoudhary@users.noreply.github.com>
2023-09-04 15:07:29 +0530

    Final commit

---
+++ main_html/logic/main.py
@@ -0,0 +1,31 @@
+import pickle
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy  as np
+
+
+app=Flask(__name__)
+data=pd.read_csv('logic\cleaned_data.csv')
+pipe=pickle.load(open("logic\RidgeModel.pk1","rb"))
+
+@app.route('/')
+
+def index():
+    locations=sorted(data['location'].unique())
+
+    return render_template('C:\FRT Project\Makaan Free Website Template - Free-CSS.com\real-estate-html-template\logic\templates\prediction.html',locations=locations)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    location=request.form.get('location')
+    bhk=request.form.get('bhk')
+    bath=request.form.get('bath')
+    sqft=request.form.get('total_sqft')
+    print(location, bhk, bath, sqft)
+    input=pd.DataFrame([[location,bhk,bath,sqft]],columns=['location','bhk','bath','total_sqft'])
+    predict=pipe.predict(input)[0] * 1e5
+
+    return str(np.round(predict,2))
+
+if __name__=="__main__":
+    app.run(debug=True,port=5001)
